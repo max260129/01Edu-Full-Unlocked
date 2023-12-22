6386f6bd3dcaf1ff37fe38a72744c778d14304a7
Diane <dianevanier76@gmail.com>
2023-12-22 01:23:45 +0100

    modif

--- src/script.js
+++ src/script.js
@@ -48,48 +48,44 @@ function convertMarkdownToHTML(markdown) {
 // Function to fetch and display a README.md file
 function displayReadme(filePath) {
     fetch(filePath)
-      .then(response => response.text())
-      .then(markdown => {
-        // Replace blockquote syntax with normal text
-        let htmlContent = marked.parse(markdown);
-        
+        .then(response => response.text())
+        .then(markdown => {
+            // Replace ```console code blocks with <pre> tags to preserve formatting
+            markdown = markdown.replace(/```console\n([\s\S]*?)```/g, '<pre class="console">$1</pre>');
+            
 
-        // Then parse the Markdown to HTML
-        const tempDiv = document.createElement('div');
-        tempDiv.innerHTML = htmlContent;
-        // Find all images in the readmeContainer and update their paths
-        const images = tempDiv.querySelectorAll('img'); // Corrected selector
-        images.forEach(image => {
-            const imagePath = image.getAttribute('src');
-            // Assuming the media files are in the same directory as the README.md
-            const newPath = new URL(imagePath, new URL(filePath, window.location.href)).href;
-            image.setAttribute('src', newPath);
+            // Parse the Markdown to HTML
+            let htmlContent = marked.parse(markdown);
+            const tempDiv = document.createElement('div');
+            tempDiv.innerHTML = htmlContent;
+
+            // Find and update image paths
+            const images = tempDiv.querySelectorAll('img');
+            images.forEach(image => {
+                const imagePath = image.getAttribute('src');
+                const newPath = new URL(imagePath, new URL(filePath, window.location.href)).href;
+                image.setAttribute('src', newPath);
+            });
+
+            // Process blockquote elements
+            const blockquotes = tempDiv.querySelectorAll('blockquote');
+            blockquotes.forEach(blockquote => {
+                const p = document.createElement('p');
+                while (blockquote.firstChild) {
+                    p.appendChild(blockquote.firstChild);
+                }
+                blockquote.parentNode.replaceChild(p, blockquote);
+            });
+
+            // Set the innerHTML of the readme container
+            document.querySelector('.readme').innerHTML = tempDiv.innerHTML;
+        })
+        .catch(error => {
+            console.error('Error fetching the README:', error);
         });
-
-          // Find all blockquote elements
-          const blockquotes = tempDiv.querySelectorAll('blockquote');
-          blockquotes.forEach(blockquote => {
-            // Create a new paragraph element
-            const p = document.createElement('p');
-            
-            // Move all children of the blockquote to the new paragraph
-            while (blockquote.firstChild) {
-              p.appendChild(blockquote.firstChild);
-            }
-            
-            // Replace blockquote with the new paragraph
-            blockquote.parentNode.replaceChild(p, blockquote);
-          });
-          
-
-          document.querySelector('.readme').innerHTML = tempDiv.innerHTML;
-
-        // Add additional processing if needed...
-      })
-      .catch(error => {
-        console.error('Error fetching the README:', error);
-      });
 }
+
+
 
 
   
